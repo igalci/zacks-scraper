6ab4e49d745909713daebe3829791cd35817c6df
igalci <igal8k@gmail.com>
2022-06-25 16:16:54 -0400

    setup

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     url='https://github.com/igalci/zacks-scraper',
     author='Igal',
     author_email='igal8k@gmail.com',
-    packages=['zacks-scraper'],
-    package_dir={'src':'zacks-scraper'},
+    packages=['src'],
+    package_dir={'':'zacks-scraper'},
     version='0.1',
     license='Apache',
     )
